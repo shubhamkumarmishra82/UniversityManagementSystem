79c253b2096fa337e1d0141a8b989fa857abd88c
shubhamkumarmishra82 <shubham9801033836@gmail.com>
2026-02-03 15:39:14 +0530

    changes in Navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -11,38 +11,48 @@ export default function Navbar(){
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-light fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Facality
+           New Information
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">add Info</a></li>
-         <li><a className="dropdown-item" href="#">Update info</a></li>
+            <li><a className="dropdown-item" href="#">Faculty</a></li>
+         <li><a className="dropdown-item" href="#">Student</a></li>
           </ul>
         </li>
          <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-light fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Student
+            View Inforamtion
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Add information</a></li>
-         <li><a className="dropdown-item" href="#">Update Inforamtion</a></li>
+            <li><a className="dropdown-item" href="#">Faculty</a></li>
+         <li><a className="dropdown-item" href="#">Student</a></li>
           </ul>
         </li>
             <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-light fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Student Leave
+            Apply Leave
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item " href="#">Add information</a></li>
-         <li><a className="dropdown-item " href="#">Update Inforamtion</a></li>
+            <li><a className="dropdown-item " href="#">Faculty Leave</a></li>
+         <li><a className="dropdown-item " href="#">Student Leave</a></li>
           </ul>
         </li>
             <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-light fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Facality leave
+            view Leave
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Add information</a></li>
-         <li><a className="dropdown-item " href="#">Update Inforamtion</a></li>
+            <li><a className="dropdown-item" href="#">faculty Leave</a></li>
+         <li><a className="dropdown-item " href="#">Student Leave</a></li>
+          </ul>
+        </li>
+
+                <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-light fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Fee
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Fees Structure</a></li>
+         <li><a className="dropdown-item " href="#">Student Fees Form</a></li>
           </ul>
         </li>
       
